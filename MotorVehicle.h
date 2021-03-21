d0a1c19e3653ac865536341e9563f203f9d598bf
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-21 17:21:27 -0400

    Update MotorVehicle.h

--- MotorVehicle.h
+++ MotorVehicle.h
@@ -1,3 +1,11 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: March 21, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+
 #ifndef SDDS_MOTORVEHICLE_H
 #define SDDS_MOTORVEHICLE_H
 
