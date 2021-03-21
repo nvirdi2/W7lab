4f508c67df6bbf5b896ca9b6e348b05f4e8afa1b
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-21 17:21:12 -0400

    Update MotorVehicle.cpp

--- MotorVehicle.cpp
+++ MotorVehicle.cpp
@@ -1,3 +1,10 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: March 21, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #include <iostream>
 #include <ostream>
 #include <cstring>
